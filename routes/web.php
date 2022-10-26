77d29003707126fcb13e8f033916dc515f577ddd
siuloirges <80370310+siuloirges@users.noreply.github.com>
2022-10-26 08:38:37 -0500

    Update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/data', [\App\Http\Controllers\ResdisConectionController::class, 'index']);
