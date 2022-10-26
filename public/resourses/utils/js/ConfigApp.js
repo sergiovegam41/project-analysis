ec103176a9a46ae39bd60d755912295f55413f05
hosting lulosys <lulosys@gmail.com>
2022-10-26 08:54:49 -0400

    Update

---
+++ public/resourses/utils/js/ConfigApp.js
@@ -0,0 +1,42 @@
+
+
+export const HOST_NAME = `${window.location.protocol}`;
+    
+export const MENU_LIST = [
+   {
+       "name" : "Home",
+       "icon" : "home-outline",
+       "url" : HOST_NAME + '/' 
+   },
+   {
+       "name" : "Partidos",
+       "icon" : "football-outline",  
+       "url" : HOST_NAME + '/resourses/modules/partidos' 
+   },
+   {
+       "name" : "Jugadores",
+       "icon" : "accessibility-outline",
+       "url" : HOST_NAME + '/resourses/modules/jugadores' 
+   },
+   {
+       "name" : "Tiempo",
+       "icon" : "calendar-number-outline",
+       "url" : HOST_NAME + '/resourses/modules/tiempo' 
+   },
+   {
+       "name" : "Mapa",
+       "icon" : "map-outline",
+       "url" : HOST_NAME + '/resourses/modules/locate' 
+   },
+   {
+       "name" : "Filtros",
+       "icon" : "options-outline",
+       "url" : HOST_NAME + '/resourses/modules/filtros' 
+   },
+//    {
+//        "name" : "Acerca de",
+//        "icon" : "information-circle-outline",
+//        "url"  : HOST_NAME + '/ProyectoElectiva/resourses/modules/acerca' 
+//     },
+
+];
