923b0985f4e5b4d50121a9d3fabb3e8e33544c6b
siuloirges <80370310+siuloirges@users.noreply.github.com>
2022-11-08 15:00:21 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ function storageImage($imagenBase64, $path=null) {
         $imageName = uniqid().'.jpg';
         $image  = base64_decode($imagenBase64);
         $path =  ($path ? $path . '/' : '');
-        \Illuminate\Support\Facades\Storage::disk('local')->put( '/public'.$path.$imageName,$image );
+        \Illuminate\Support\Facades\Storage::disk('local')->put( '/'.$path.$imageName,$image );
         $url = '/storage'.$path.$imageName;
         return $url;
 
